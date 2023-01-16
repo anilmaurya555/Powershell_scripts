50ebc574892c1f45c4777865a7c40270e7e5dab9
anilmaurya555 <117301170+anilmaurya555@users.noreply.github.com>
2023-01-15 20:15:16 -0500

    Add files via upload

---
+++ testing_nested_hash_table_0115.ps1
@@ -0,0 +1,266 @@
+﻿$clusterstats = @{
+    chyusnpccp03 = @{
+                         'NPC Netapp napsdccp138v_prod'= @{
+                                                              nolongerneeded =  ''
+                                                              status =  'kRunning'
+                                                              starttime=  '01/14/2023 08=00PM'
+                                                              clients=  {
+
+                                                                          }
+                                                          }
+                         'napsdccp500v_CIFS_new_prod'= @{
+                                                            nolongerneeded =  ''
+                                                            'status'=  'kRunning'
+                                                            'starttime'=  '01/13/2023 07=00PM'
+                                                            'clients'=  {
+
+                                                                        }
+                                                        }
+                         'NPC Netapp napsdccp137v_prod'= @{
+                                                              'nolongerneeded'=  ''
+                                                              'status'=  'null'
+                                                              'starttime'=  '01/11/2023 09=00PM'
+                                                              'clients'=  @{
+                                                                              'wfs_prod_home'=  99
+                                                                          }
+                                                          }
+                         'NPC Netapp napsdccp500v_prod'= @{
+                                                              'nolongerneeded'=  ''
+                                                              'status'=  'kRunning'
+                                                              'starttime'=  '01/13/2023 10=00PM'
+                                                              'clients'=  {
+
+                                                                          }
+                                                          }
+                         'NPC Netapp napsdccp352v_prod'=  @{
+                                                              'nolongerneeded'=  ''
+                                                              'status'=  'kRunning'
+                                                              'starttime'=  '01/14/2023 07=00PM'
+                                                              'clients'=  {
+
+                                                                          }
+                                                          }
+                     }
+}
+$htmlFileName = "Latest_replication_status.html"
+
+#################HTML#############
+$html = '<html>
+<head>
+    <style>
+                h1 {
+            background-color:#b1ffb1;
+            }
+        p {
+            color: #555555;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        span {
+            color: #555555;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        
+        table {
+            font-family: Arial, Helvetica, sans-serif;
+            color: #333333;
+            font-size: 0.75em;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        tr {
+            border: 1px solid #F1F1F1;
+        }
+        td,
+        th {
+            width: 33%;
+            text-align: left;
+            padding: 6px;
+        }
+        tr:nth-child(even) {
+            background-color: #F1F1F1;
+        }
+    </style>
+</head>
+<body>
+    
+    <div style="margin:15px;">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARgAAAAoCAMAAAASXRWnAAAC8VBMVE
+            WXyTz///+XyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTwJ0VJ2AAAA+nRSTlMAAAECAwQFBgcICQoLDA0ODxARExQVFhcYGRobHB0eHy
+            EiIyQlJicoKSorLC0uLzAxMjM0NTY3ODk6Ozw9Pj9AQUNERUZHSElKS0xNTk9QUVJTVFVWV1hZWl
+            tcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9foCBgoOEhYaHiImKi4yNjo+QkZKTlJ
+            WWl5iZmpucnZ6foKGio6SlpqeoqaqrrK2ur7CxsrO0tba3uLm6u7y9vr/AwcLDxMXGx8jJysvMzc
+            7Q0dLT1NXW19jZ2tvc3d7f4OHi4+Xm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+drbbjAAACOZJRE
+            FUaIHtWmlcVUUUv6alIgpiEGiZZIpiKu2i4obhUgipmGuihuZWiYmkRBu4JJVappaG5VJRUWrllq
+            ZWivtWVuIWllHwShRI51PvnjP33pk7M1d579Gn/j8+zDnnf2b5v3tnu2g1/ocUmvuPRasx83cVu1
+            zFB5endtWUCHgoM/+0y1V64sOZcXVlhMDpWXdLM+PmPnmdZTVJeLCPiL6Jd9jT6nfo2y+hH4vE/h
+            Fcj6bP6uhcqxvxfYzOdsxOb6gYm39qdrRmE6bBxB2EQWHOXfLBvVvMsIqWdBEYzYvcgWRJ6nS3f5
+            +/YSWXEQVeYJPqpXx5XkaaalFuOu22h2E5UVkrIadaAyXFXTwbKh1cw0J3bCgvzFO/CRWtuk3IjP
+            lKYK23C7ga3IFCblPwp1HrNvUAyH1W0tRzKlIbk/OmbpbX04uNHGp1/9j6MxMMxUNSYXbqoTJWmF
+            t3yCqqHGVLzJK2l8qTtoOzldBqD/C/Ra3hDgOYZKTU2awmpZgVbwG7udWGEvovHYXFHIkuYzHECN
+            Pzb0VNy9g8/60KVh5X/QbwtRCajQH//GsQ5k7KCTzqQGprVrwW7HC9GOKQQMhpP30UpWiIM0XYZQ
+            gcsYR50Mo9vj73vS9+sOy1Vl6A5S7auXJ53v4Lpr2Trf9LcN0utNsZ/K9Ra4iy++XGE+h3zGGQaV
+            bFn+n2lWZQ7q/6id04iW/fI2idFTp4CAOdTWHuNFWZQCf7luMOGr4e9jxCXu1WBxw3Ja03XJs8FG
+            ZFdBcbusY2NRKM2k9mD32oXwKLxIGRTMWsMFpon14PAGKTynX/9z17ot27Z23KxyeMLLT1bw6hHT
+            SECaTLTOWUmgxt3B/ofcxwLKfdXM2+JH0MtTI8E2aqwLLQDWsuH3+9A0kHJwwDWKC2ifwAF9Z8L+
+            dtj87TmikMnTkONOfTg/PAHU7NUVSBQbZWcqjf2vhURZiXHMZ7BBi/RzhQEAphQi7q/l2ShA7Y5S
+            L2QdDOoDPSFCYBHQfF3+UZQlwDaDkAJybSSWBl0FZMh4+EuRcIl8Qtg4AqC6NlY58/Zlyvo2uaZg
+            rzEz6wN0ryWyY2tlU1TML6CENDDdtHwswCQpqaYKLqwmg/Y5/7mo5O6Niil1GYOPQMkOab8MMN5Q
+            fSIO5Mjxumj4T5To+X3gDlsUuXvQV4e0nOyEg70wNhInDUZfWp7Y8rbBnsy1EYnKI3SdMt4AxDu2
+            kHfRmjqekbYWrrBwuSD+V3CIc9k7jJwRNhtCewqnXUpAtgHBggjP8l8EQpO4hYB6xsRfQ4ROdQyz
+            fChELHZuvFaGLHsWiW6okwdBtKEsHoj8YKDIEwuLf7Udk/RL2/FINFPAbRvdTyjTA3/6PHM/Vioi
+            AMITMYqkfCNMDJ4aJ+mgwAJjlXC0MgTKbjo2AAd/OHVeHQSj1cQedvFKamwGoqEeYpZZMBJXp8iV
+            4MPCNR5mWL6pEwWi9i/pybsWgcS0GYfHD1V/YPMQZYi5Vx3HLcjwYKk9I7nkdcmkSY9x/gSQnx5j
+            r4ox7HQ3D4nkvlFwEXyk1lzJ2nh8JouVjP49pELEw2AiDMCfDdp8xGzASWeun8AOIJrDAqXO2sdC
+            GeEnAXQG+tQpuEAUIad3/uF8ps4qUw1+NqWjIEp9lvzAAIg5NHc2U2Yh6wRirj8yE+2hfCkMtBSB
+            hh664JP9zhkI2Gw0NhtPvZZisamX4QBtbvypvV2YDFkPuIMj4X4mPR8FIY0h4J9XGvLbs3GY9EYx
+            fuqTBaGtMqs5GzhLlytX03PhGPKuOvQNw3T0ypselagPYrkvbwNVtBLY+F0faYra5mvCAMvrD3OG
+            W78TywnlbGcQf2MBreCfOzeRprUIGeYynCmx4Ac/B5uvJ5LkzoFdrqSdYLwuC14NVWJZy31avStx
+            DvgAYKM6pbLx5dpkiEWdqmPYeoqFpWrb1NtY4fPAQ4fHQb3g+tAXekt8Jow2gD3EUsCIPTqtPp3+
+            qi/ALZjbowhVcGs8KIp4dmEmGmOTb7hOyRAjUmQJE+ol4IQzs7l/OBMDj3H3XO1kJwIgxXhHGvdI
+            Bry/v7GDcmS4RZpAf6QjEZWd4Ikw4VDeZ8IEwTbK2dczoedUmWIsrL7kNhtO7M9TMF3EjGQ5HuH7
+            wRBpf+8ZwPT9c4Ma+/SgfxNsol7vN1tMYeGx8DfSmMdl1GoU0Y2LjjS0Z3lN4IM1spDL6t9MCtxK
+            3IypUG4TMVKTRMnwqjabV6ZeVtK9i9S0fBnny8QsXTPl2tqkcYnDit3QOLO1KHG0V6TTdQwkrFUL
+            Jh+1gYGfA8eoZa1SOMfrOr4zsxKcnt/pyWW9AHub3AisXAb6bjPxBmMyQvpVY1CUPPUmSD/Wszbp
+            jHUGsRsspibawkqlhv01P9wryITRq3a9UkjHlBVsR9GemAM4e1Vza+IOWwAoYto97Zlq8qwjzj3G
+            0pwldikysNR3UJo42mgyNfD6pDY7F5hs88OQZXUs/5LGM/E5ljfKXdztRbFWFyAkPsaOxvpQS1im
+            jBITxiaO4/2OSVgGoXRnvZUIH8smHetPR566wlcpXFjzGdZO+KjKmZq8zPuOSon4fCVJSU2VHx60
+            wjI6OEqGEdY6pPGC1T1Tq3V+5UqmBtYXWh18yiMDGcMMMUdekYgpQRDhT2UhQ/dCiE2X0twkxQCa
+            MNKJY1XtyPr+WWDdI+PsuztoGztdAHXL6WUGukw6ALkPKJmnF5OFPxRnAJv0QYuA/Y3TwW2FW2Ca
+            OFrRFbXxMm1PP0nwJrXw8bB7/RiF82W4LfOFa0dRDmDaTMVRK2cv+nh10X/oXLD64sdzgLg2eleM
+            5n+x+8Tu9wg3Yt6yyrqFH6Ea6LXyQJFFjlMiW5S93+YlPsl5TDPkbHGLxfGi7J58ehtdO9MzQBcN
+            HXXaEIRZB+GCvgv9sL/7UZNGjhzlMlLtefhdsXDG6kqRCd9tnh8y5X6dmC3NHS83a73LX2/4lATN
+            64iLlEjZk8aaIETyZb3Rw9Y3oah/Rp42KDhHqj3v18hKy9AZ+u6Sjzs6g/e1NGbd5Vo8a/916SKO
+            8LK0YAAAAASUVORK5CYII=" style="width:180px">
+        <p style="margin-top: 15px; margin-bottom: 15px;">
+            <span style="font-size:1.3em;">'
+
+
+$html += '</span>
+<span style="font-size:0.75em; text-align: right; padding-top: 8px; padding-right: 2px; float: right;">'
+$html += $date
+
+$html += '</span>
+                </p>
+                <table>
+                <tr>
+                        <th>Cluster Name</th>
+                        <th>Job Nmae</th>
+                        <th>Job Start Time</th>
+                        <th>Status</th>
+                        <th>Client Replicating</th>
+                        <th>Percentage Completed</th>
+                        <th>Replication Needed</th>
+                        </tr>'
+                 
+                $clusterstats.GetEnumerator()|sort-object -Property {$_.key} |foreach {  ##2
+
+                ############################################################################
+
+                                    $cluster = $_.key
+                                    
+                                    $html += "<tr>
+                                                 <td>$cluster</td>"
+                                    $jobs = @() 
+                                foreach ($job in $_.value.keys){
+                                                $jobs += $job
+                
+                                                        }
+                                                      
+                                if ($jobs.count -gt 0){
+                                foreach ($job in $jobs){
+                                $gotcluster = $false
+                                $clusterstats.$($cluster).$($job).GetEnumerator()|foreach {
+                                $_.Value.status
+                                            $starttime = $_.value.starttime
+                                           
+                                            $status    = $_.value.status
+                                            $nolongerneeded = $_.value.nolongerneeded
+                                            if (!$gotcluster -eq $true){
+                                            $html += "
+                                                 <td>$job</td>
+                                                 <td>$starttime</td>
+                                                 <td>$status</td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td>$nolongerneeded</td>
+                                                 </tr>"
+                                                                        }else {
+                                                                        $html += "
+                                                <td></td>
+                                                 <td>$job</td>
+                                                 <td>$starttime</td>
+                                                 <td>$status</td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td>$nolongerneeded</td>
+                                                 </tr>"
+                                                                        }
+                                                 $gotcluster = $true
+
+                                                                                      
+                                                                                      }
+
+                                   
+                                if (($clusterstats.$($cluster).$($job).clients.keys).count -gt 0){
+                                                                 
+                                                                 $clusterstats.$($cluster).$($job).clients.GetEnumerator()|foreach {
+                                                                $client = $_.key
+                                                                $pct    = $_.value
+                                                                $html += "<tr>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td>$client</td>
+                                                 <td>$pct</td>
+                                                 <td></td>
+                                                 </tr>"
+                                      }
+                                                                       }
+                                                      }
+                                                      }else {
+
+                                                      $html += "<tr>
+                                                 <td></td>
+                                                 <td>No active tasks found</td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 </tr>"
+                                                      
+
+                                                      }
+
+                ############################################################################                          
+
+
+                                    }
+
+                                    
+                                                     
+                             
+                                
+                                    
+
+                                     $html += "
+</table>                
+</div>
+</body>
+</html>"
+
+$html | out-file $htmlFileName
