ae11b454b2ebeeca626143da6e232b8a27ea2d75
anilmaurya555 <117301170+anilmaurya555@users.noreply.github.com>
2023-02-23 13:42:12 -0500

    Add files via upload

---
+++ principals_excel_html.ps1
@@ -0,0 +1,285 @@
+﻿### process commandline arguments
+[CmdletBinding()]
+param (
+    #[Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter()][string]$domain = 'ent.ad.ntrs.com', #local or AD domain
+    [Parameter()][switch]$admin 
+)
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+
+#"ClusterNmae,AccountType,Doamin,Name,Permission"|Out-File -FilePath ./accountpermissions.csv
+
+$vips = ('chyukrccp01','chyukpccp01','chyididcp01')
+#$vips = ('chyididcp01','chymaidcp01','chysgpccp01','chysgrccp01','chyukpccp01','chyukrccp01','chyusnpccp01','chyuswpccp01','chyusnpccp02','chyuswpccp02','chyusnpccp03','chyuswpccp03','chyusnpccp04','chyuswpccp04','chyusnpccp05','chyuswpccp05')
+$permisions = @{}
+
+#################HTML#############
+$html = '<html>
+<head>
+    <style>
+                h1 {
+            background-color:#b1ffb1;
+            }
+        p {
+            color: #555555;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        span {
+            color: #555555;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        
+        table, th, td {
+  border: 2px solid green;
+  border-collapse: collapse;
+  font-size: .9em;
+  width: 100%;
+   }
+th, td {
+  background-color: #96D4D4;
+  width: 33%;
+            text-align: left;
+            padding: 6px;
+}
+        tr:nth-child(even) {
+            background-color: #F1F1F1;
+        }
+
+        .cluster { font-weight: bold;
+                   color: #008000;
+                   font-size: 1.5em}
+    </style>
+</head>
+<body>
+    
+    <div style="margin:15px;">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARgAAAAoCAMAAAASXRWnAAAC8VBMVE
+            WXyTz///+XyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyTyXyT
+            yXyTyXyTyXyTyXyTwJ0VJ2AAAA+nRSTlMAAAECAwQFBgcICQoLDA0ODxARExQVFhcYGRobHB0eHy
+            EiIyQlJicoKSorLC0uLzAxMjM0NTY3ODk6Ozw9Pj9AQUNERUZHSElKS0xNTk9QUVJTVFVWV1hZWl
+            tcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9foCBgoOEhYaHiImKi4yNjo+QkZKTlJ
+            WWl5iZmpucnZ6foKGio6SlpqeoqaqrrK2ur7CxsrO0tba3uLm6u7y9vr/AwcLDxMXGx8jJysvMzc
+            7Q0dLT1NXW19jZ2tvc3d7f4OHi4+Xm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+drbbjAAACOZJRE
+            FUaIHtWmlcVUUUv6alIgpiEGiZZIpiKu2i4obhUgipmGuihuZWiYmkRBu4JJVappaG5VJRUWrllq
+            ZWivtWVuIWllHwShRI51PvnjP33pk7M1d579Gn/j8+zDnnf2b5v3tnu2g1/ocUmvuPRasx83cVu1
+            zFB5endtWUCHgoM/+0y1V64sOZcXVlhMDpWXdLM+PmPnmdZTVJeLCPiL6Jd9jT6nfo2y+hH4vE/h
+            Fcj6bP6uhcqxvxfYzOdsxOb6gYm39qdrRmE6bBxB2EQWHOXfLBvVvMsIqWdBEYzYvcgWRJ6nS3f5
+            +/YSWXEQVeYJPqpXx5XkaaalFuOu22h2E5UVkrIadaAyXFXTwbKh1cw0J3bCgvzFO/CRWtuk3IjP
+            lKYK23C7ga3IFCblPwp1HrNvUAyH1W0tRzKlIbk/OmbpbX04uNHGp1/9j6MxMMxUNSYXbqoTJWmF
+            t3yCqqHGVLzJK2l8qTtoOzldBqD/C/Ra3hDgOYZKTU2awmpZgVbwG7udWGEvovHYXFHIkuYzHECN
+            Pzb0VNy9g8/60KVh5X/QbwtRCajQH//GsQ5k7KCTzqQGprVrwW7HC9GOKQQMhpP30UpWiIM0XYZQ
+            gcsYR50Mo9vj73vS9+sOy1Vl6A5S7auXJ53v4Lpr2Trf9LcN0utNsZ/K9Ra4iy++XGE+h3zGGQaV
+            bFn+n2lWZQ7q/6id04iW/fI2idFTp4CAOdTWHuNFWZQCf7luMOGr4e9jxCXu1WBxw3Ja03XJs8FG
+            ZFdBcbusY2NRKM2k9mD32oXwKLxIGRTMWsMFpon14PAGKTynX/9z17ot27Z23KxyeMLLT1bw6hHT
+            SECaTLTOWUmgxt3B/ofcxwLKfdXM2+JH0MtTI8E2aqwLLQDWsuH3+9A0kHJwwDWKC2ifwAF9Z8L+
+            dtj87TmikMnTkONOfTg/PAHU7NUVSBQbZWcqjf2vhURZiXHMZ7BBi/RzhQEAphQi7q/l2ShA7Y5S
+            L2QdDOoDPSFCYBHQfF3+UZQlwDaDkAJybSSWBl0FZMh4+EuRcIl8Qtg4AqC6NlY58/Zlyvo2uaZg
+            rzEz6wN0ryWyY2tlU1TML6CENDDdtHwswCQpqaYKLqwmg/Y5/7mo5O6Niil1GYOPQMkOab8MMN5Q
+            fSIO5Mjxumj4T5To+X3gDlsUuXvQV4e0nOyEg70wNhInDUZfWp7Y8rbBnsy1EYnKI3SdMt4AxDu2
+            kHfRmjqekbYWrrBwuSD+V3CIc9k7jJwRNhtCewqnXUpAtgHBggjP8l8EQpO4hYB6xsRfQ4ROdQyz
+            fChELHZuvFaGLHsWiW6okwdBtKEsHoj8YKDIEwuLf7Udk/RL2/FINFPAbRvdTyjTA3/6PHM/Vioi
+            AMITMYqkfCNMDJ4aJ+mgwAJjlXC0MgTKbjo2AAd/OHVeHQSj1cQedvFKamwGoqEeYpZZMBJXp8iV
+            4MPCNR5mWL6pEwWi9i/pybsWgcS0GYfHD1V/YPMQZYi5Vx3HLcjwYKk9I7nkdcmkSY9x/gSQnx5j
+            r4ox7HQ3D4nkvlFwEXyk1lzJ2nh8JouVjP49pELEw2AiDMCfDdp8xGzASWeun8AOIJrDAqXO2sdC
+            GeEnAXQG+tQpuEAUIad3/uF8ps4qUw1+NqWjIEp9lvzAAIg5NHc2U2Yh6wRirj8yE+2hfCkMtBSB
+            hh664JP9zhkI2Gw0NhtPvZZisamX4QBtbvypvV2YDFkPuIMj4X4mPR8FIY0h4J9XGvLbs3GY9EYx
+            fuqTBaGtMqs5GzhLlytX03PhGPKuOvQNw3T0ypselagPYrkvbwNVtBLY+F0faYra5mvCAMvrD3OG
+            W78TywnlbGcQf2MBreCfOzeRprUIGeYynCmx4Ac/B5uvJ5LkzoFdrqSdYLwuC14NVWJZy31avStx
+            DvgAYKM6pbLx5dpkiEWdqmPYeoqFpWrb1NtY4fPAQ4fHQb3g+tAXekt8Jow2gD3EUsCIPTqtPp3+
+            qi/ALZjbowhVcGs8KIp4dmEmGmOTb7hOyRAjUmQJE+ol4IQzs7l/OBMDj3H3XO1kJwIgxXhHGvdI
+            Bry/v7GDcmS4RZpAf6QjEZWd4Ikw4VDeZ8IEwTbK2dczoedUmWIsrL7kNhtO7M9TMF3EjGQ5HuH7
+            wRBpf+8ZwPT9c4Ma+/SgfxNsol7vN1tMYeGx8DfSmMdl1GoU0Y2LjjS0Z3lN4IM1spDL6t9MCtxK
+            3IypUG4TMVKTRMnwqjabV6ZeVtK9i9S0fBnny8QsXTPl2tqkcYnDit3QOLO1KHG0V6TTdQwkrFUL
+            Jh+1gYGfA8eoZa1SOMfrOr4zsxKcnt/pyWW9AHub3AisXAb6bjPxBmMyQvpVY1CUPPUmSD/Wszbp
+            jHUGsRsspibawkqlhv01P9wryITRq3a9UkjHlBVsR9GemAM4e1Vza+IOWwAoYto97Zlq8qwjzj3G
+            0pwldikysNR3UJo42mgyNfD6pDY7F5hs88OQZXUs/5LGM/E5ljfKXdztRbFWFyAkPsaOxvpQS1im
+            jBITxiaO4/2OSVgGoXRnvZUIH8smHetPR566wlcpXFjzGdZO+KjKmZq8zPuOSon4fCVJSU2VHx60
+            wjI6OEqGEdY6pPGC1T1Tq3V+5UqmBtYXWh18yiMDGcMMMUdekYgpQRDhT2UhQ/dCiE2X0twkxQCa
+            MNKJY1XtyPr+WWDdI+PsuztoGztdAHXL6WUGukw6ALkPKJmnF5OFPxRnAJv0QYuA/Y3TwW2FW2Ca
+            OFrRFbXxMm1PP0nwJrXw8bB7/RiF82W4LfOFa0dRDmDaTMVRK2cv+nh10X/oXLD64sdzgLg2eleM
+            5n+x+8Tu9wg3Yt6yyrqFH6Ea6LXyQJFFjlMiW5S93+YlPsl5TDPkbHGLxfGi7J58ehtdO9MzQBcN
+            HXXaEIRZB+GCvgv9sL/7UZNGjhzlMlLtefhdsXDG6kqRCd9tnh8y5X6dmC3NHS83a73LX2/4lATN
+            64iLlEjZk8aaIETyZb3Rw9Y3oah/Rp42KDhHqj3v18hKy9AZ+u6Sjzs6g/e1NGbd5Vo8a/916SKO
+            8LK0YAAAAASUVORK5CYII=" style="width:180px">
+        <p style="margin-top: 15px; margin-bottom: 15px;">
+            <span style="font-size:1.3em;">'
+
+foreach ($vip in $vips){
+
+$privileges = @()
+$newpriv    = @()
+if ($vip -notin $permisions.keys){
+$permisions[$vip] = @{}
+$permisions[$vip]['Users']=@{}
+$permisions[$vip]['Groups']=@{}
+}
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+"============= $vip ================"
+
+$cluster = api get cluster
+$outFile = "principals-$($cluster.name).txt"
+$null = Remove-Item -Path $outFile -Force -ErrorAction SilentlyContinue
+
+$roles = api get roles
+$users = api get users?_includeTenantInfo=true
+$groups = api get groups?_includeTenantInfo=true
+$parents = @{}
+$privileges = ($roles |where {$_.label -eq "Admin"}).privileges
+foreach ($pri in $privileges){
+
+         $newpriv += "$pri,"
+                            }
+
+foreach($user in $users | Sort-Object -Property username){
+            if ($user.domain -notin $permisions[$vip]['Users'].keys){
+            $permisions[$vip]['Users'][$user.domain] = @{} }
+
+         if($user.PSObject.Properties['roles']){
+        $proles = $roles | Where-Object name -in $user.roles
+        
+        if($user.username){
+           $pname = $user.username}else{ $pname = $user.name}
+           $ptype = "User"
+           $descrip = $user.description
+           $permisions[$vip]['Users'][$user.domain][$pname] = @{}
+           #"`n{0}: {1}/{2}" -f $ptype.ToUpper(), $user.domain, $pname
+          #"Roles: {0}" -f $proles.label -join ', '
+        $permisions[$vip]['Users'][$user.domain][$pname]['permission'] = $proles.label -join ', '
+        $permisions[$vip]['Users'][$user.domain][$pname]['description']= $descrip
+        
+        
+      
+      
+    }  ##look for roles
+}
+
+foreach($group in $groups | Sort-Object -Property name){
+    if ($group.domain -notin $permisions[$vip]['Groups'].keys){
+    $permisions[$vip]['Groups'][$group.domain] = @{}}
+     if($group.PSObject.Properties['roles']){
+        $proles = $roles | Where-Object name -in $group.roles
+        
+
+         if($group.username){
+           $pname = $group.username}else{ $pname = $group.name}
+           $ptype = "Group"
+           $descrip = $group.description
+           $permisions[$vip]['Groups'][$group.domain][$pname] = @{}
+           #"`n{0}: {1}/{2}" -f $ptype.ToUpper(), $group.domain, $pname
+           #"Roles: {0}" -f $proles.label -join ', '
+        $permisions[$vip]['Groups'][$group.domain][$pname]['permission'] = $proles.label -join ', '
+       
+        $permisions[$vip]['Groups'][$group.domain][$pname]['description'] = $descrip        
+       
+    }  ##look for roles
+}
+
+}
+
+$html += '<table>
+                <tr> 
+                <td>'
+$permisions.GetEnumerator()|%{
+$vip = $_.name
+
+$html += '</span>
+                </p>
+                <table >
+                <tr style=padding-left: 5px; font-size:15px>
+                
+                        <th>Cluster Nmae</th>
+                        <th>Type</th>
+                        <th>Domain</th>
+                        <th>Account</th>
+                        <th>Description</th>
+                        <th>Permissions</th>
+                        </tr>'
+$html += "
+                                                 <td class='cluster'>$vip</td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 <td></td>
+                                                 </tr>"
+
+foreach ($cluster in $_.value.GetEnumerator()){  ####type
+  $type = $cluster.name
+
+  foreach ($dom in $cluster.value.GetEnumerator()){ ###dom
+       $domain = $dom.name
+            foreach ($prin in $dom.value.GetEnumerator()){
+            
+              if($admin){
+                if ($prin.value.permission -like "*Admin*"){
+                "{0} {1} {2} {3} {4} " -f $vip,$type,$prin.name,$prin.value.description,  $prin.value.permission    
+                
+                
+                
+                $html += "
+                                                 <td></td>
+                                                 <td>$type</td>
+                                                 <td>$domain</td>
+                                                 <td>$($prin.name)</td>
+                                                 <td>$($prin.value.description)</td>
+                                                 <td>$($prin.value.permission)</td>
+                                                 </tr>" }
+                        }else{
+                       "{0} {1} {2} {3} " -f $vip,$type,$prin.name,$prin.value  
+
+                        $html += "
+                                                 <td></td>
+                                                 <td>$type</td>
+                                                 <td>$domain</td>
+                                                 <td>$($prin.name)</td>
+                                                 <td>$($prin.value.description)</td>
+                                                 <td>$($prin.value.permission)</td>
+                                                 </tr>"
+                        }            
+ 
+#"$vip,$type,$domain,$($prin.name),$($prin.value) "|Out-File -FilePath ./accountpermissions.csv -Append
+
+
+                                                         }
+
+                                               }    ##dom
+
+                                             }   ###type
+
+                        $html += "
+                                                 </td>
+                                                 </table>"
+                                                 
+
+                            }
+$html += "<td>             <!start of second cell in frist row>
+<table >          <! start of right table>
+<tr><th>Privileges</th></tr>
+<tr><td >$newpriv</td></tr>
+</table>         <! end of right table >
+</td>            <! end of main cell which was divided between two table>
+</tr>            <! end of 1st row>
+</table>         <! end of main table>
+</body>
+</html>"
+
+$outfilepath = "Account_privileges.html"
+$html | Out-File -FilePath $outfilepath
