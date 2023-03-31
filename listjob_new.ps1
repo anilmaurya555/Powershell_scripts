73727b94a1b30da593a9a185002aab1764431086
anilmaurya555 <117301170+anilmaurya555@users.noreply.github.com>
2023-03-31 15:47:15 -0500

    Update listjob_new.ps1

--- listjob_new.ps1
+++ listjob_new.ps1
@@ -1,15 +1,62 @@
-﻿[cmdletbinding()]
+[cmdletbinding()]
 param (
       [parameter ( mandatory = $True)][string]$vip,
-      [parameter ()][array]$prefix = 'All',
+      [parameter ()][array]$prefix ,
       [parameter ()] [switch] $count,
+      [Parameter ()][array]$jobNames, #jobs for which user wants to list/cancel replications
+      [Parameter ()][string]$joblist = '',
       [parameter ()] [switch] $detailjob,
       [parameter ()] [string] $bypolicy,
       [parameter ()] [switch] $listAlljobnames
       )
 . .\cohesity-api.ps1
+$outFile = $(Join-Path -Path $PSScriptRoot -ChildPath "out.txt")
+
+function gatherList($Param=$null, $FilePath=$null, $Required=$True, $Name='items'){
+    $items = @()
+    if($Param){
+        $Param | ForEach-Object {$items += $_}
+    }
+    if($FilePath){
+        if(Test-Path -Path $FilePath -PathType Leaf){
+            Get-Content $FilePath | ForEach-Object {$items += [string]$_}
+        }else{
+            Write-Host "Text file $FilePath not found!" -ForegroundColor Yellow
+            exit
+        }
+    }
+    if($Required -eq $True -and $items.Count -eq 0){
+        Write-Host "No $Name specified" -ForegroundColor Yellow
+        exit
+    }
+    return ($items | Sort-Object -Unique)
+}
+
+$alljobNames = @(gatherList -Param $jobNames -FilePath $jobList -Name 'jobs' -Required $false)
+
+$jobs = @()
 apiauth -vip $vip -username aym15-sa -domain ent.ad.ntrs.com
-$jobs = (api get protectionJobs | Where-Object {$_.isdeleted -ne $True -and $_.isactive -ne $false -and $_.isPaused -eq $false})
+$rawjobs = (api get protectionJobs | Where-Object {$_.isdeleted -ne $True -and $_.isactive -ne $false -and $_.isPaused -eq $false})
+if ($jobNames -or $jobList){
+    foreach ($job in $rawjobs){
+            if ($job.name -in $alljobNames){
+                         $jobs += $job
+                                        }
+
+                                  }
+     
+
+                           }elseif ($prefix){
+                         foreach ($job in $rawjobs){
+                           if ($job.name.tolower() -like "$prefix*" -or $job.name.tolower() -like "*$prefix*" -or $job.name.tolower() -like "*$prefix"){
+                                         $jobs += $job
+                                                    }                                                  
+
+                                                  }
+                                            }else {
+
+                                            $jobs = $rawjobs
+                                            }
 
 $policies = api get protectionPolicies
 if ($listAlljobnames){
@@ -38,14 +85,15 @@ if ($listAlljobnames){
                   
                   
                   }elseif($detailjob){
-                  "Job Nmae                                                       Vcenter Nmae                     Policy Name               start Time"
-                  "===================================================================================================================================="
+                  "Job Nmae                                                       Vcenter Nmae                     Policy Name                 Client Count       start Time"
+                  "========================================================================================================================================================="
                   $parentsources = api get protectionSources
                   #$parentsources |ConvertTo-Json -Depth 25
                   forEach ($job in $jobs ){
                     $jobname= $job.name
                     $starttime = "$($job.startTime.hour):$($job.startTime.minute)"
-                     
+                                  #$job|ConvertTo-Json -Depth 10 |Out-File -FilePath $outFile
+                                                       
                     foreach ($ptsource in $parentsources){
 
                     if ($ptsource.protectionSource.id -eq $job.parentSourceId){
@@ -58,7 +106,7 @@ if ($listAlljobnames){
                                      $policyname = $policy.name.Tolower()
                                      }
                                   }
-                    "{0,-60}  {1,-30}  {2,-35}  {3,-20}" -f $jobname,$vcentername,$policyname,$starttime
+                    "{0,-60}  {1,-30}  {2,-35}  {3,-10}  {4,-20}" -f $jobname,$vcentername,$policyname,($job.sourceIds).count,$starttime
                                   
                                   }   ###each job
                     
@@ -70,7 +118,7 @@ if ($listAlljobnames){
                     forEach ($job in $jobs ){
                             $jobname= $job.name
                             #$jobname
-                             if ($jobname.tolower() -like "$prefix*" -or $jobname.tolower() -like "*$prefix*" -or $jobname.tolower() -like "*$prefix" -or $prefix -eq 'ALL') {
+                             
                               $clients = @()
                                  $newcount = 0
                                      $report = api get reports/protectionSourcesJobsSummary?jobIds=$($job.id)
@@ -87,7 +135,7 @@ if ($listAlljobnames){
                                                              "`nClinets list"
                                                              $clients
                                                                   }                                   
-                                                }
+                                                
                                          }
                          }
                          
